eda0732729bf6c5d7cd9aceb228dafa6666b4c27
Shlomo Bluth <lomibluth@gmail.com>
2021-09-05 11:09:40 +0300

    add commit

--- cypress/integration/requestsTests.js
+++ cypress/integration/requestsTests.js
@@ -4,100 +4,113 @@
 
 ////run tests on requests from citation run some in hebrew mode and english mode
 
-let sizes = ['iphone-x',[1000, 660]]
+const urls = new Map();
+urls.set('live',Cypress.env('LIVE_URL'))
+urls.set('dev',Cypress.env('DEV_URL')) 
+
+const sizes= new Map();
+sizes.set('desktop',[1000, 660])
+sizes.set('mobile','iphone-x')
 
 
-sizes.forEach((size) => {
+urls.forEach((urlValue,urlKey)=>{
 
-    describe('RequestsTests',()=>{
+    sizes.forEach((sizeValue,sizeKey) => {
+
     
-        beforeEach(() => {
-            cy.screenSize({size:size})
-            cy.visitpage({url:'https://citation.dicta.org.il/'})
-        })
+        describe('toolTests '+urlKey+' '+sizeKey,()=>{
+    
+            beforeEach(() => {
+                cy.screenSize({size:sizeValue})
+                cy.visitpage({url:urlValue})
+            })
+
+            it('Error message for markpsukim response with status code 500 when clicking the run button of citation page'+
+            ' in hebrew mode',()=>{
+               cy.citationRequest({
+                   url:'markpsukim',
+                   language:'Hebrew',
+                   status:500,
+                   message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר',
+               })
+            })
+        
+            it('Error message for markpsukim response with status code 500 when clicking the run button of citation page'+
+            ' in english mode',()=>{
+                cy.citationRequest({
+                    url:'markpsukim',
+                    language:'English',
+                    status:500,
+                    message:'Server error. Please try again later',
+                })
+            })
+        
+            it('Error message for markpsukim response with a delay of 15 seconds when clicking the run button'+
+            ' of citation page in hebrew mode',()=>{
+                cy.citationRequest({
+                    url:'markpsukim',
+                    language:'Hebrew',
+                    message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר',
+                    delaySeconds: 60
+                })
+            })
+        
+            it('Error message for markpsukim response with a delay of 15 seconds when clicking the run button'+
+            ' of citation page in english mode',()=>{
+                cy.citationRequest({
+                    url:'markpsukim',
+                    language:'English',
+                    message:'Server error. Please try again later',
+                    delaySeconds: 60
+                })
+            })
+        
+            // it('Error message for parsetogroups response with status code 500 when clicking the run button of citation page'+
+            // ' in hebrew mode',()=>{
+            //    cy.citationRequest({
+            //        url:'parsetogroups',
+            //        language:'Hebrew',
+            //        status:500,
+            //        message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר',
+            //    })
+            // })
+        
+            // it('Error message for parsetogroups response with status code 500 when clicking the run button of citation page'+
+            // ' in english mode',()=>{
+            //     cy.citationRequest({
+            //         url:'parsetogroups',
+            //         language:'English',
+            //         status:500,
+            //         message:'Server error. Please try again later',
+            //     })
+            // })
+        
+            // it('Error message for parsetogroups response with a delay of 15 seconds when clicking the run button'+
+            // ' of citation page in hebrew mode',()=>{
+            //     cy.citationRequest({
+            //         url:'parsetogroups',
+            //         language:'Hebrew',
+            //         message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר',
+            //         delaySeconds: 60
+            //     })
+            // })
+        
+            // it('Error message for parsetogroups response with a delay of 15 seconds when clicking the run button'+
+            // ' of citation page in english mode',()=>{
+            //     cy.citationRequest({
+            //         url:'parsetogroups',
+            //         language:'English',
+            //         message:'Server error. Please try again later',
+            //         delaySeconds: 60
+            //     })
+            // })
+            
+
       
     
-        it('Error message for markpsukim response with status code 500 when clicking the run button of citation page'+
-        ' in hebrew mode',()=>{
-           cy.citationRequest({
-               url:'markpsukim',
-               language:'Hebrew',
-               status:500,
-               message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר',
-           })
-        })
-    
-        it('Error message for markpsukim response with status code 500 when clicking the run button of citation page'+
-        ' in english mode',()=>{
-            cy.citationRequest({
-                url:'markpsukim',
-                language:'English',
-                status:500,
-                message:'Server error. Please try again later',
-            })
-        })
-    
-        it('Error message for markpsukim response with a delay of 15 seconds when clicking the run button'+
-        ' of citation page in hebrew mode',()=>{
-            cy.citationRequest({
-                url:'markpsukim',
-                language:'Hebrew',
-                message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר',
-                delaySeconds: 60
-            })
-        })
-    
-        it('Error message for markpsukim response with a delay of 15 seconds when clicking the run button'+
-        ' of citation page in english mode',()=>{
-            cy.citationRequest({
-                url:'markpsukim',
-                language:'English',
-                message:'Server error. Please try again later',
-                delaySeconds: 60
-            })
-        })
-    
-        // it('Error message for parsetogroups response with status code 500 when clicking the run button of citation page'+
-        // ' in hebrew mode',()=>{
-        //    cy.citationRequest({
-        //        url:'parsetogroups',
-        //        language:'Hebrew',
-        //        status:500,
-        //        message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר',
-        //    })
-        // })
-    
-        // it('Error message for parsetogroups response with status code 500 when clicking the run button of citation page'+
-        // ' in english mode',()=>{
-        //     cy.citationRequest({
-        //         url:'parsetogroups',
-        //         language:'English',
-        //         status:500,
-        //         message:'Server error. Please try again later',
-        //     })
-        // })
-    
-        // it('Error message for parsetogroups response with a delay of 15 seconds when clicking the run button'+
-        // ' of citation page in hebrew mode',()=>{
-        //     cy.citationRequest({
-        //         url:'parsetogroups',
-        //         language:'Hebrew',
-        //         message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר',
-        //         delaySeconds: 60
-        //     })
-        // })
-    
-        // it('Error message for parsetogroups response with a delay of 15 seconds when clicking the run button'+
-        // ' of citation page in english mode',()=>{
-        //     cy.citationRequest({
-        //         url:'parsetogroups',
-        //         language:'English',
-        //         message:'Server error. Please try again later',
-        //         delaySeconds: 60
-        //     })
-        // })
-    
         
+    
+        })
     })
 })
 
--- cypress/integration/toolTests.js
+++ cypress/integration/toolTests.js
@@ -2,32 +2,43 @@
 
 //run basic tests on citation run
 
-let sizes = ['iphone-x',[1000, 660]]
+const urls = new Map();
+urls.set('live',Cypress.env('LIVE_URL'))
+urls.set('dev',Cypress.env('DEV_URL')) 
+
+const sizes= new Map();
+sizes.set('desktop',[1000, 660])
+sizes.set('mobile','iphone-x')
 
 
-sizes.forEach((size) => {
+urls.forEach((urlValue,urlKey)=>{
 
-    describe('toolTests',()=>{
+    sizes.forEach((sizeValue,sizeKey) => {
 
-        beforeEach(() => {
-            cy.screenSize({size:size})
-            cy.visitpage({url:'https://citation.dicta.org.il/'})
-        })
     
-        it('citation run in hebrew mode',()=>{
-            cy.setLanguageMode('Hebrew')
-            cy.intercept('**/api/markpsukim**').as('req')
-            cy.findCitation('משה קבל תורה מסיני ומסרה ליהושע')
-            cy.wait('@req')
-            cy.resultsTests(' משֶׁה קִבֵּל תּוֹרָה מִסִּינַי וּמְסָרָהּ לִיהוֹשֻׁעַ וִיהוֹשֻׁעַ לִזְקֵנִים וּזְקֵנִים')
-        })
+        describe('toolTests '+urlKey+' '+sizeKey,()=>{
     
-        it('citation run in english mode',()=>{
-            cy.setLanguageMode('English')
-            cy.intercept('**/api/markpsukim**').as('req')
-            cy.findCitation('משה קבל תורה מסיני ומסרה ליהושע')
-            cy.wait('@req')
-            cy.resultsTests(' משֶׁה קִבֵּל תּוֹרָה מִסִּינַי וּמְסָרָהּ לִיהוֹשֻׁעַ וִיהוֹשֻׁעַ לִזְקֵנִים וּזְקֵנִים')
+            beforeEach(() => {
+                cy.screenSize({size:sizeValue})
+                cy.visitpage({url:urlValue})
+            })
+
+            it('citation run in hebrew mode',()=>{
+                cy.setLanguageMode('Hebrew')
+                cy.intercept('**/api/markpsukim**').as('req')
+                cy.findCitation('משה קבל תורה מסיני ומסרה ליהושע')
+                cy.wait('@req')
+                cy.resultsTests(' משֶׁה קִבֵּל תּוֹרָה מִסִּינַי וּמְסָרָהּ לִיהוֹשֻׁעַ וִיהוֹשֻׁעַ לִזְקֵנִים וּזְקֵנִים')
+            })
+        
+            it('citation run in english mode',()=>{
+                cy.setLanguageMode('English')
+                cy.intercept('**/api/markpsukim**').as('req')
+                cy.findCitation('משה קבל תורה מסיני ומסרה ליהושע')
+                cy.wait('@req')
+                cy.resultsTests(' משֶׁה קִבֵּל תּוֹרָה מִסִּינַי וּמְסָרָהּ לִיהוֹשֻׁעַ וִיהוֹשֻׁעַ לִזְקֵנִים וּזְקֵנִים')
+            })
+
         })
     })
 
