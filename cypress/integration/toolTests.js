5238b7ca93a401d5c3f92ef9178deb8d6f5278de
Shlomo Bluth <lomibluth@gmail.com>
2021-09-06 12:41:36 +0300

    add commit

--- cypress/integration/toolTests.js
+++ cypress/integration/toolTests.js
@@ -4,11 +4,11 @@
 
 const urls = new Map();
 urls.set('live',Cypress.env('LIVE_URL'))
-//urls.set('dev',Cypress.env('DEV_URL')) 
+urls.set('dev',Cypress.env('DEV_URL')) 
 
 const sizes= new Map();
 sizes.set('desktop',[1000, 660])
-//sizes.set('mobile','iphone-x')
+sizes.set('mobile','iphone-x')
 
 
 urls.forEach((urlValue,urlKey)=>{
